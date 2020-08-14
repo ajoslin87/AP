1e21453db4d6b08212f3751a0e0b61fbf8aae598
ajoslin87 <ajoslin87@gmail.com>
2020-08-13 21:00:11 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+function changeText(id){
+    id.innerHTML = "AP CONTRACTING";
+}
+
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+}
+
+$(function(){
+    $(".toggle").on("click", function(){
+        if($(".item").hasClass("active")){
+            $(".item" ).removeClass("active");
+        }
+        else{
+            $(".item").addClass("active");
+    }
+    })
+})
